da556a07d01411d4b1b41e0ca876d8de06cdd87c
Yunzhong Hou <hou_yz@outlook.com>
2021-07-02 15:59:42 +1000

    remove unused bash scripts

--- train_classifiers.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 python grid_downsample.py -d cifar10 -a alexnet --train
-CUDA_VISIBLE_DEVICES=0 python grid_downsample.py -d cifar100 -a alexnet --train
-CUDA_VISIBLE_DEVICES=0 python grid_downsample.py -d stl10 -a alexnet --train
-CUDA_VISIBLE_DEVICES=0 python grid_downsample.py -d tiny200 -a alexnet --train
