45dc12042889da63c8ef635c2ca419d4befec0c1
Yunzhong Hou <hou_yz@outlook.com>
2021-08-17 23:41:38 +1000

    bottleneck=0 support

--- color_distillation/models/color_cnn.py
+++ color_distillation/models/color_cnn.py
@@ -25,8 +25,12 @@ class ColorCNN(nn.Module):
             self.base = StyleUNet()
         else:
             raise Exception
-        self.bottleneck = nn.Sequential(nn.Conv2d(self.base.out_channel, bottleneck_channel, 1),
-                                        nn.BatchNorm2d(bottleneck_channel), nn.ReLU(), )
+        if bottleneck_channel != 0:
+            self.bottleneck = nn.Sequential(nn.Conv2d(self.base.out_channel, bottleneck_channel, 1),
+                                            nn.BatchNorm2d(bottleneck_channel), nn.ReLU(), )
+        else:
+            self.bottleneck = nn.Sequential()
+            bottleneck_channel = self.base.out_channel
         # support color quantization into 256 colors at most
         self.color_mask = nn.Conv2d(bottleneck_channel, colors_channel, 1, bias=False)
         self.agg = agg
@@ -64,18 +68,25 @@ class ColorCNN(nn.Module):
 if __name__ == '__main__':
     from color_distillation import datasets
     import color_distillation.utils.transforms as T
-    from color_distillation.loss.similarity_preserving import *
+    from color_distillation.loss.similarity_preserving import PixelSimLoss
 
-    num_colors = 2
-    dataset = datasets.STL10('/home/houyz/Data/stl10', split='test', color_quantize=T.MedianCut(),
-                             transform=T.ToTensor())
+    num_colors = 16
+    dataset = datasets.CIFAR10('/home/houyz/Data/cifar10', color_quantize=T.MedianCut(), transform=T.ToTensor())
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, num_workers=0, pin_memory=True)
     dataloader.dataset.num_colors[0] = num_colors
     img, target = next(iter(dataloader))
     img, label, quantized_img, index_map = img.cuda(), target[0].cuda(), target[1][0].cuda(), target[1][1].cuda()
     B, C, H, W = img.shape
-    model = ColorCNN('styleunet').cuda()
-    trans_img, m, color_palette = model(img, num_colors, mode='test')
+    model = ColorCNN('unet').cuda()
+    model.load_state_dict(torch.load('/home/houyz/Code/colorcnn_plus/logs/colorcnn/cifar10/resnet18/-64colors/'
+                                     'unet_aggmean_neck16_colors256_topk4_ce1.0_kd0.0_pixsim10.0_recons0.0_prcp0.0_'
+                                     'max1.0_conf1.0_info1.0_post_jit1.0_norm4.0_2021-06-28_16-43-19/ColorCNN.pth'))
+    trans_img, m, color_palette = model(img, num_colors, mode='train')
+    M = torch.zeros_like(m).scatter(1, index_map, 1)
+    loss_1 = PixelSimLoss(1, normalize=False)(m, M)
+    loss_2 = PixelSimLoss(normalize=False)(m, M)
+    loss_3 = PixelSimLoss(1, normalize=True)(m, M)
+    loss_4 = PixelSimLoss(normalize=True)(m, M)
     crop = T.RandomCrop(32, padding=4)
     crop(trans_img)
     pass
