c3bfb6f24e9ecc22fbb5568cb400332087d12f25
Yunzhong Hou <hou_yz@outlook.com>
2021-08-17 23:43:43 +1000

    train on OG images and support for style14

--- grid_downsample.py
+++ grid_downsample.py
@@ -8,10 +8,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
 from torchvision import datasets
 import color_distillation.utils.transforms as T
 from color_distillation import models
 from color_distillation.trainer import CNNTrainer
+from color_distillation.utils.sampler import RandomSeqSampler
 from color_distillation.utils.load_checkpoint import checkpoint_loader
 from color_distillation.utils.draw_curve import draw_curve
 from color_distillation.utils.logger import Logger
@@ -27,17 +29,6 @@ def main(args):
         torch.backends.cudnn.benchmark = False
     else:
         torch.backends.cudnn.benchmark = True
-
-    if args.dataset == 'svhn' or args.dataset == 'cifar10' or args.dataset == 'cifar100':
-        H, W, C = 32, 32, 3
-    elif args.dataset == 'imagenet':
-        H, W, C = 224, 224, 3
-    elif args.dataset == 'stl10':
-        H, W, C = 96, 96, 3
-    elif args.dataset == 'tiny200':
-        H, W, C = 64, 64, 3
-    else:
-        raise Exception
 
     buffer_size_counter = BufferSizeCounter()
     if args.sample_name == 'mcut':
@@ -56,7 +47,7 @@ def main(args):
         sample_trans = [T.JpegCompression(buffer_size_counter, args.jpeg_ratio)]
         test_sample_trans = T.JpegCompression(buffer_size_counter, args.jpeg_ratio)
     elif args.sample_name is None:
-        sample_trans = [T.PNGCompression(buffer_size_counter)]
+        sample_trans = []
         test_sample_trans = T.PNGCompression(buffer_size_counter)
         args.sample_name = 'og_img'
     else:
@@ -64,82 +55,83 @@ def main(args):
 
     # dataset
     data_path = os.path.expanduser('~/Data/') + args.dataset
-    normalize = T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    mean_var = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     test_sample_trans = T.Compose([T.ToPILImage(), test_sample_trans, T.ToTensor()])
-    if args.dataset == 'svhn':
-        num_class = 10
-
-        sampled_train_trans = T.Compose(sample_trans + [T.ToTensor(), normalize, ])
-        og_test_trans = T.Compose([T.ToTensor(), ])
-        sampled_test_trans = T.Compose(sample_trans + [T.ToTensor(), ])
-
-        sampled_train_set = datasets.SVHN(data_path, split='train', download=True, transform=sampled_train_trans)
-        og_test_set = datasets.SVHN(data_path, split='test', download=True, transform=og_test_trans)
-        sampled_test_set = datasets.SVHN(data_path, split='test', download=True, transform=sampled_test_trans)
-    elif args.dataset == 'cifar10' or args.dataset == 'cifar100':
+    base_lr_ratio = 1
+    if args.dataset == 'cifar10' or args.dataset == 'cifar100':
         num_class = 10 if args.dataset == 'cifar10' else 100
 
-        sampled_train_trans = T.Compose(sample_trans + [T.RandomCrop(32, padding=4),
-                                                        T.RandomHorizontalFlip(), T.ToTensor(), normalize, ])
+        og_train_trans = T.Compose([T.RandomCrop(32, padding=4), T.RandomHorizontalFlip(), T.ToTensor(), ])
         og_test_trans = T.Compose([T.ToTensor(), ])
         sampled_test_trans = T.Compose(sample_trans + [T.ToTensor(), ])
 
         if args.dataset == 'cifar10':
-            sampled_train_set = datasets.CIFAR10(data_path, train=True, download=True, transform=sampled_train_trans)
+            og_train_set = datasets.CIFAR10(data_path, train=True, download=True, transform=og_train_trans)
             og_test_set = datasets.CIFAR10(data_path, train=False, download=True, transform=og_test_trans)
             sampled_test_set = datasets.CIFAR10(data_path, train=False, download=True, transform=sampled_test_trans)
         else:
-            sampled_train_set = datasets.CIFAR100(data_path, train=True, download=True, transform=sampled_train_trans)
+            og_train_set = datasets.CIFAR100(data_path, train=True, download=True, transform=og_train_trans)
             og_test_set = datasets.CIFAR100(data_path, train=False, download=True, transform=og_test_trans)
             sampled_test_set = datasets.CIFAR100(data_path, train=False, download=True, transform=sampled_test_trans)
     elif args.dataset == 'imagenet':
         num_class = 1000
+        args.batch_size = 32
 
-        sampled_train_trans = T.Compose(sample_trans + [T.RandomResizedCrop(224), T.RandomHorizontalFlip(),
-                                                        T.ToTensor(), normalize, ])
+        og_train_trans = T.Compose([T.RandomResizedCrop(224), T.RandomHorizontalFlip(), T.ToTensor(), ])
         og_test_trans = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor(), ])
         sampled_test_trans = T.Compose(sample_trans + [T.Resize(256), T.CenterCrop(224), T.ToTensor(), ])
 
-        sampled_train_set = datasets.ImageNet(data_path, split='train', transform=sampled_train_trans, )
+        og_train_set = datasets.ImageNet(data_path, split='train', transform=og_train_trans, )
         og_test_set = datasets.ImageNet(data_path, split='val', transform=og_test_trans)
         sampled_test_set = datasets.ImageNet(data_path, split='val', transform=sampled_test_trans, )
+    elif args.dataset == 'style14mini':
+        num_class = 14
+        args.batch_size = 32
+        base_lr_ratio = 0.1
+        args.lr = 0.01
+
+        og_train_trans = T.Compose([T.RandomResizedCrop(112), T.RandomHorizontalFlip(), T.ToTensor(), ])
+        og_test_trans = T.Compose([T.Resize(128), T.CenterCrop(112), T.ToTensor(), ])
+        sampled_test_trans = T.Compose(sample_trans + [T.Resize(128), T.CenterCrop(112), T.ToTensor(), ])
+
+        og_train_set = datasets.ImageFolder(data_path + '/train', transform=og_train_trans, )
+        og_test_set = datasets.ImageFolder(data_path + '/val', transform=og_test_trans)
+        sampled_test_set = datasets.ImageFolder(data_path + '/val', transform=sampled_test_trans, )
     elif args.dataset == 'stl10':
         num_class = 10
         # smaller batch size
         args.batch_size = 32
 
-        sampled_train_trans = T.Compose(sample_trans + [T.RandomCrop(96, padding=12),
-                                                        T.RandomHorizontalFlip(), T.ToTensor(), normalize, ])
+        og_train_trans = T.Compose([T.RandomCrop(96, padding=12), T.RandomHorizontalFlip(), T.ToTensor(), ])
         og_test_trans = T.Compose([T.ToTensor(), ])
         sampled_test_trans = T.Compose(sample_trans + [T.ToTensor(), ])
 
-        sampled_train_set = datasets.STL10(data_path, split='train', download=True, transform=sampled_train_trans)
+        og_train_set = datasets.STL10(data_path, split='train', download=True, transform=og_train_trans)
         og_test_set = datasets.STL10(data_path, split='test', download=True, transform=og_test_trans)
         sampled_test_set = datasets.STL10(data_path, split='test', download=True, transform=sampled_test_trans)
     elif args.dataset == 'tiny200':
         num_class = 200
 
-        sampled_train_trans = T.Compose(sample_trans + [T.RandomCrop(64, padding=8), T.RandomHorizontalFlip(),
-                                                        T.ToTensor(), normalize, ])
+        og_train_trans = T.Compose([T.RandomCrop(64, padding=8), T.RandomHorizontalFlip(), T.ToTensor(), ])
         og_test_trans = T.Compose([T.ToTensor(), ])
         sampled_test_trans = T.Compose(sample_trans + [T.ToTensor(), ])
 
-        sampled_train_set = datasets.ImageFolder(data_path + '/train', transform=sampled_train_trans, )
+        og_train_set = datasets.ImageFolder(data_path + '/train', transform=og_train_trans, )
         og_test_set = datasets.ImageFolder(data_path + '/val', transform=og_test_trans)
         sampled_test_set = datasets.ImageFolder(data_path + '/val', transform=sampled_test_trans, )
     else:
         raise Exception
 
-    sampled_train_loader = torch.utils.data.DataLoader(sampled_train_set, batch_size=args.batch_size, shuffle=True,
-                                                       num_workers=args.num_workers, pin_memory=True)
-    og_test_loader = torch.utils.data.DataLoader(og_test_set, batch_size=args.batch_size * 2, shuffle=False,
-                                                 num_workers=args.num_workers, pin_memory=True)
-    sampled_test_loader = torch.utils.data.DataLoader(sampled_test_set, batch_size=args.batch_size * 2, shuffle=False,
-                                                      num_workers=args.num_workers, pin_memory=True)
+    og_train_loader = DataLoader(og_train_set, batch_size=args.batch_size, shuffle=True,
+                                 num_workers=args.num_workers, pin_memory=True)
+    og_test_loader = DataLoader(og_test_set, batch_size=args.batch_size * 2,
+                                # sampler=RandomSeqSampler(og_test_set),
+                                num_workers=args.num_workers, pin_memory=True)
+    sampled_test_loader = DataLoader(sampled_test_set, batch_size=args.batch_size * 2,
+                                     # sampler=RandomSeqSampler(og_test_set),
+                                     num_workers=args.num_workers, pin_memory=True)
 
     logdir = 'logs/grid/{}/{}/{}colors'.format(args.dataset, args.arch,
-                                               'full_' if args.num_colors is None else args.num_colors)
+                                               'full_')  # if args.num_colors is None else args.num_colors
     if args.train:
         os.makedirs(logdir, exist_ok=True)
         sys.stdout = Logger(os.path.join(logdir, 'log.txt'), )
@@ -147,11 +139,16 @@ def main(args):
     print(vars(args))
 
     # model
-    model = models.create(args.arch, num_class, not args.train).cuda()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    model = models.create(args.arch, num_class).cuda()
+    param_dicts = [{"params": [p for n, p in model.named_parameters() if
+                               'classifier' not in n and 'fc' not in n and p.requires_grad],
+                    "lr": args.lr * base_lr_ratio},
+                   {"params": [p for n, p in model.named_parameters() if
+                               ('classifier' in n or 'fc' in n) and p.requires_grad], }]
+    optimizer = optim.SGD(param_dicts, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr,
-                                                    steps_per_epoch=len(sampled_train_loader), epochs=args.epochs)
-    # scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, args.step_size, 1)
+                                                    steps_per_epoch=len(og_train_loader), epochs=args.epochs)
+    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size)
 
     # draw curve
     x_epoch = []
@@ -162,14 +159,14 @@ def main(args):
     masked_test_loss_s = []
     masked_test_prec_s = []
 
-    trainer = CNNTrainer(args, model, mean_var=mean_var, sample_name=args.sample_name)
+    trainer = CNNTrainer(args, model, logdir=logdir, sample_name=args.sample_name)
 
     # learn
     if args.train:
         for epoch in tqdm(range(1, args.epochs + 1)):
             print('Train on sampled dateset...')
-            train_loss, train_prec = trainer.train(epoch, sampled_train_loader, optimizer,
-                                                   log_interval=args.log_interval, cyclic_scheduler=scheduler)
+            train_loss, train_prec = trainer.train(epoch, og_train_loader, optimizer, cyclic_scheduler=scheduler)
+            # scheduler.step()
             print('Test on original dateset...')
             og_test_loss, og_test_prec = trainer.test(og_test_loader)
             print('Test on sampled dateset...')
@@ -188,15 +185,15 @@ def main(args):
         torch.save(model.state_dict(), os.path.join(logdir, 'model.pth'))
     else:
         if args.dataset != 'imagenet':
-            resume_dir = 'logs/grid/{}/{}/full_colors'.format(args.dataset, args.arch)
-            resume_fname = resume_dir + '/model.pth'
-            model.load_state_dict(torch.load(resume_fname))
+            model.load_state_dict(torch.load(logdir + '/model.pth'))
     pass
     # test
     model.eval()
     # without adversarial
     print('Test on sampled dateset...')
-    trainer.test(sampled_test_loader, args.num_colors, visualize=args.visualize)
+    img, tgt = next(iter(og_test_loader))
+    B, C, H, W = img.shape
+    trainer.test(sampled_test_loader, args.num_colors, args.epochs, visualize=args.visualize)
     print(f'Average image size: {buffer_size_counter.size / len(sampled_test_set):.1f}; '
           f'Bit per pixel: {buffer_size_counter.size / len(sampled_test_set) / H / W:.3f}')
     buffer_size_counter.reset()
@@ -204,7 +201,7 @@ def main(args):
     if args.adversarial:
         print('********************    [adversarial first]    ********************')
         trainer = CNNTrainer(args, model, adversarial=args.adversarial, epsilon=args.epsilon,
-                             mean_var=mean_var, sample_name=args.sample_name, sample_trans=test_sample_trans)
+                             sample_name=args.sample_name, sample_trans=test_sample_trans)
         print(f'Test on sampled dateset [adversarial: {args.adversarial} @ epsilon: {args.epsilon}]...')
         trainer.test(og_test_loader, args.num_colors, visualize=args.visualize)
         print(f'Average image size: {buffer_size_counter.size / len(sampled_test_set):.1f}; '
@@ -229,21 +226,22 @@ if __name__ == '__main__':
                         choices=['mcut', 'octree', 'kmeans', 'jpeg'])
     parser.add_argument('--dither', action='store_true', default=False)
     parser.add_argument('--jpeg_ratio', type=int, default=None)
+    parser.add_argument('--label_smooth', type=float, default=0.0)
     parser.add_argument('--train', action='store_true', default=False)
     parser.add_argument('--adversarial', default=None, type=str, choices=['fgsm', 'deepfool', 'bim', 'cw'])
     parser.add_argument('--epsilon', default=4, type=int)
     parser.add_argument('-d', '--dataset', type=str, default='cifar10',
-                        choices=['cifar10', 'cifar100', 'stl10', 'svhn', 'imagenet', 'tiny200'])
+                        choices=['cifar10', 'cifar100', 'stl10', 'style14mini', 'imagenet', 'tiny200'])
     parser.add_argument('-a', '--arch', type=str, default='vgg16', choices=models.names())
     parser.add_argument('-j', '--num_workers', type=int, default=4)
-    parser.add_argument('-b', '--batch_size', type=int, default=128, metavar='N',
+    parser.add_argument('-b', '--batch_size', type=int, default=128,
                         help='input batch size for training (default: 128)')
-    parser.add_argument('--epochs', type=int, default=60, metavar='N', help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.1, metavar='LR', help='learning rate (default: 0.1)')
-    parser.add_argument('--step_size', type=int, default=20, metavar='N', help='step_size for training (default: 10)')
+    parser.add_argument('--epochs', type=int, default=60, help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.1, help='learning rate (default: 0.1)')
+    parser.add_argument('--step_size', type=int, default=30)
     parser.add_argument('--weight_decay', type=float, default=5e-4)
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M', help='SGD momentum (default: 0.5)')
-    parser.add_argument('--log_interval', type=int, default=100, metavar='N',
+    parser.add_argument('--momentum', type=float, default=0.9, help='SGD momentum')
+    parser.add_argument('--log_interval', type=int, default=100,
                         help='how many batches to wait before logging training status')
     parser.add_argument('--visualize', action='store_true')
     parser.add_argument('--seed', type=int, default=None, help='random seed (default: None)')
