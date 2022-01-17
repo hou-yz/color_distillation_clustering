97dd01c4786188228e1260836b62ee4e6d79e1c2
Yunzhong Hou <hou_yz@outlook.com>
2022-01-17 16:50:39 +1100

    square pad

--- color_distillation/utils/transforms.py
+++ color_distillation/utils/transforms.py
@@ -1,8 +1,20 @@
+import numpy as np
+import torchvision.transforms.functional as F
 from PIL import Image
 from io import BytesIO
 from color_distillation.utils.dither.palette import Palette
 from color_distillation.utils.dither.dithering import error_diffusion_dithering
 from torchvision.transforms import *
+
+
+class SquarePad:
+    def __call__(self, image):
+        w, h = image.size
+        max_wh = np.max([w, h])
+        hp = int((max_wh - w) / 2)
+        vp = int((max_wh - h) / 2)
+        padding = (hp, vp, hp, vp)
+        return F.pad(image, padding, 0, 'constant')
 
 
 class MedianCut(object):
