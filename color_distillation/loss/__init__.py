1562437e67b7af7c4b5b68ddc142f0908ede7cbf
Yunzhong Hou <hou_yz@outlook.com>
2023-04-19 12:25:14 +1000

    remove unused

--- color_distillation/loss/__init__.py
+++ color_distillation/loss/__init__.py
@@ -1,4 +1,2 @@
-from .entropy_loss import HLoss
 from .knowledge_distillation import KDLoss
-from .label_smooth import LabelSmoothLoss
 from .similarity_preserving import PixelSimLoss, ChannelSimLoss, BatchSimLoss
